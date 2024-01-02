67f0e7b9a0b4fa84b362c020292b6592a687b019
Emily Wertheimer <71396046+Emily-Wertheimer@users.noreply.github.com>
2024-01-02 14:33:43 -0500

    Create troubleshooting_sbatch_job_submission.sh

---
+++ troubleshooting_sbatch_job_submission.sh
@@ -0,0 +1,6 @@
+# check file persmissions
+ls -l /gpfs/gibbs/pi/huckins/ekw28/ghre_lep_sema_RNAseq/align.R
+
+
+# make file executable
+chmod u+x /gpfs/gibbs/pi/huckins/ekw28/ghre_lep_sema_RNAseq/align.R
