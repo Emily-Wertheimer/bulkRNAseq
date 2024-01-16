4f8bd29747d0596d1d7aeca08a37c2cca54eaea4
Emily Wertheimer <71396046+Emily-Wertheimer@users.noreply.github.com>
2024-01-16 11:35:31 -0500

    Delete troubleshooting_sbatch_job_submission.sh

--- troubleshooting_sbatch_job_submission.sh
+++
@@ -1,6 +0,0 @@
-# check file persmissions
-ls -l /gpfs/gibbs/pi/huckins/ekw28/ghre_lep_sema_RNAseq/align.R
-
-
-# make file executable
-chmod u+x /gpfs/gibbs/pi/huckins/ekw28/ghre_lep_sema_RNAseq/align.R
